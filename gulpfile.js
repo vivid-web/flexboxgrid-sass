298addd24dd4b348c749383fee972f1b298fadae
Peter van Meijgaard <petervanmeijgaard@hotmail.com>
2016-07-19 22:33:12 +0200

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -4,27 +4,27 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var prefix = require('gulp-autoprefixer');
-var minifyCss = require('gulp-minify-css');
+var cleanCss = require('gulp-clean-css');
 var rename = require('gulp-rename');
 var jade = require('gulp-jade');
 
 var directories = {
     sass: {
-        input: './scss/flexbox-grid.scss',
+        input: './src/flexbox-grid.scss',
         output: {
             folder: './dist',
             fileName: 'flexbox-grid.css'
         }
     },
     jade: {
-        input: './resources/jade/index.jade',
+        input: './assets/jade/index.jade',
         output: {
             folder: './docs',
             fileName: 'index.html'
         }
     },
     minify: {
-        input: './scss/flexbox-grid.scss',
+        input: './src/flexbox-grid.scss',
         output: {
             folder: './dist',
             fileName: 'flexbox-grid.min.css'
@@ -32,7 +32,8 @@ var directories = {
     }
 };
 
-gulp.task('sass', function () {
+// Dist
+gulp.task('dist-css', function() {
     return gulp
         .src(directories.sass.input)
         .pipe(sourcemaps.init())
@@ -43,33 +44,41 @@ gulp.task('sass', function () {
         .pipe(gulp.dest(directories.sass.output.folder));
 });
 
-gulp.task('minify-css', function () {
+gulp.task('dist-minify', function() {
     return gulp
         .src(directories.minify.input)
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(prefix())
-        .pipe(minifyCss())
+        .pipe(cleanCss())
         .pipe(rename(directories.minify.output.fileName))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(directories.minify.output.folder));
 });
 
+gulp.task('build', ['dist-css', 'dist-minify']);
 
 
-gulp.task('docs-css', function () {
+// Docs
+gulp.task('docs-fonts', function() {
     return gulp
-        .src(['./resources/scss/stylesheet.scss'])
+        .src(['./assets/fonts/**'])
+        .pipe(gulp.dest('./docs/dist/fonts/'));
+});
+
+gulp.task('docs-css', function() {
+    return gulp
+        .src(['./assets/scss/stylesheet.scss'])
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(prefix())
-        .pipe(minifyCss())
+        .pipe(cleanCss())
         .pipe(rename('stylesheet.min.css'))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./docs/dist'));
+        .pipe(gulp.dest('./docs/dist/css'));
 });
 
-gulp.task('docs-html', function () {
+gulp.task('docs-html', function() {
     return gulp
         .src(directories.jade.input)
         .pipe(jade({
@@ -79,4 +88,6 @@ gulp.task('docs-html', function () {
         .pipe(gulp.dest(directories.jade.output.folder));
 });
 
-gulp.task('default', ['sass', 'minify-css', 'docs-css', 'docs-html']);
+gulp.task('docs', ['docs-fonts', 'docs-css', 'docs-html']);
+
+gulp.task('default', ['build', 'docs']);
